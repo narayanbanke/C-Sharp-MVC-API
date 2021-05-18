9f79d6d9433249ffe2ec2ec4a42e8e00fe9a0076
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-05-18 20:21:33 +0530

    added

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SOLID
-{ // updated 
-    class Program
+{
+    public class UserService
     {
-        static void Main(string[] args)
+        public static void Register(string email, string password)
         {
+            if (!ValidateEmail(email))
+                throw new ValidationException("Email is not an email");
+            user user = new user(email, password);
+
+            SendEmail(new MailMessage("mysite@nowhere.com", email) { Subject = "HEllo foo" });
+        }
+        public static bool ValidateEmail(string email)
+        {
+            return email.Contains("@");
+        }
+        public static bool SendEmail(MailMessage message)
+        {
+            SmtpClient _smtpClient = new SmtpClient();
+            _smtpClient.Send(message);
+            return true;
+        }
+
+        public static void Main()
+        {
+            Register("", "");
+        }
+        public class user
+        {
+            public string email { get; set; }
+            public string password { get; set; }
+            public user(string email, string password)
+            {
+                this.email = email;
+                this.password = password;
+            }
         }
     }
 }
