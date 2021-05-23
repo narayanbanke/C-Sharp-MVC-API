396d7536b2e212880f01546035c9084e2c6c4a01
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-05-23 19:23:45 +0530

    added

---
+++ ControlOverTheDependentObjectCreation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ControlOverTheDependentObjectCreation
+{
+    public class A
+    {
+        B b;
+        public A()
+        {
+            b = new B();
+        }
+        public void Task1()
+        {
+            // do something here..
+            b.SomeMethod();
+            // do something here..
+        }
+    }
+    public class B
+    {
+        public void SomeMethod()
+        {
+            //doing something..
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CustomerBusinessLogicIOC/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerBusinessLogicIOC
+{
+    class Program
+    {
+        public class CustomerBusinessLogic
+        {
+            DataAccess _dataAccess;
+
+            public CustomerBusinessLogic()
+            {
+                _dataAccess = new DataAccess();
+            }
+
+            public string GetCustomerName(int id)
+            {
+                return _dataAccess.GetCustomerName(id);
+            }
+        }
+
+        public class DataAccess
+        {
+            public DataAccess()
+            {
+            }
+
+            public string GetCustomerName(int id)
+            {
+                return "Dummy Customer Name"; // get it from DB in real app
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
