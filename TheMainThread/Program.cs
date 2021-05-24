c2e0bddfa7bfb7c3d7cb11fb23a8d9fff9cfcb80
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-05-24 19:33:42 +0530

    added

---
+++ TheMainThread/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TheMainThread
+{
+ 
+   class MainThreadProgram
+    {
+        static void Main(string[] args)
+        {
+            Thread th = Thread.CurrentThread;
+            th.Name = "MainThread";
+
+            Console.WriteLine("This is {0}", th.Name);
+            Console.ReadKey();
+        }
+    }
+}
