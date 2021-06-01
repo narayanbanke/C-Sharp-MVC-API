bb1e7130b5858915aba78c78e50b2761c29be3ca
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-06-01 07:27:42 +0530

    adedded

--- TPL/Program.cs
+++ TPL/Program.cs
@@ -7,35 +7,48 @@ using System.Threading.Tasks;
 
 namespace TPL
 {
-    
 
-    
-        class Program
+
+
+    class Program
+    {
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                Console.WriteLine("C# For Loop");
-                int number = 10;
-                for (int count = 0; count < number; count++)
-                {
-                    //Thread.CurrentThread.ManagedThreadId returns an integer that 
-                    //represents a unique identifier for the current managed thread.
-                    Console.WriteLine($"value of count = {count}, thread = {Thread.CurrentThread.ManagedThreadId}");
-                    //Sleep the loop for 10 miliseconds
-                    Thread.Sleep(10);
-                }
-                Console.WriteLine();
+            Console.WriteLine("C# For Loop");
+            Thread thread = new Thread(Runmillianitraton);
 
-                Console.WriteLine("Parallel For Loop");
-                Parallel.For(0, number, count =>
-                {
-                    Console.WriteLine($"value of count = {count}, thread = {Thread.CurrentThread.ManagedThreadId}");
-                    //Sleep the loop for 10 miliseconds
-                    Thread.Sleep(10);
-                });
-                Console.ReadLine();
+            //int number = 10000000;
+            //for (int count = 0; count < number; count++)
+            //{
+            //    //Thread.CurrentThread.ManagedThreadId returns an integer that 
+            //    //represents a unique identifier for the current managed thread.
+            //    Console.WriteLine($"value of count = {count}, thread = {Thread.CurrentThread.ManagedThreadId}");
+            //    //Sleep the loop for 10 miliseconds
+            //    Thread.Sleep(10);
+            //}
+            Console.WriteLine();
+
+            //Console.WriteLine("Parallel For Loop");
+            //Parallel.For(0, number, count =>
+            //{
+            //    Console.WriteLine($"value of count = {count}, thread = {Thread.CurrentThread.ManagedThreadId}");
+            //    //Sleep the loop for 10 miliseconds
+            //    Thread.Sleep(10);
+            //});
+            //Console.ReadLine();
+        }
+        private static void Runmillianitraton()
+        {
+            string x = "";
+
+            for (int i = 0; i < 1000000; i++)
+            { 
+            
             }
         }
-    
-    
+
+
+    }
 }
+    
+ 
