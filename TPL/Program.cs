0741003947241fc3743fa0f3eebabef6bd2f1134
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-06-02 18:56:00 +0530

    updated

--- TPL/Program.cs
+++ TPL/Program.cs
@@ -15,27 +15,27 @@ namespace TPL
         static void Main(string[] args)
         {
             Console.WriteLine("C# For Loop");
-            Thread thread = new Thread(Runmillianitraton);
+          // Thread thread = new Thread(Runmillianitraton);
 
-            //int number = 10000000;
+            int number = 10000000;
             //for (int count = 0; count < number; count++)
             //{
-            //    //Thread.CurrentThread.ManagedThreadId returns an integer that 
-            //    //represents a unique identifier for the current managed thread.
+            //    Thread.Current//Thread.ManagedThreadId returns an integer that 
+            //    represents a unique identifier for the current managed thread.
             //    Console.WriteLine($"value of count = {count}, thread = {Thread.CurrentThread.ManagedThreadId}");
-            //    //Sleep the loop for 10 miliseconds
+            //    Sleep the loop for 10 miliseconds
             //    Thread.Sleep(10);
             //}
             Console.WriteLine();
 
-            //Console.WriteLine("Parallel For Loop");
-            //Parallel.For(0, number, count =>
-            //{
-            //    Console.WriteLine($"value of count = {count}, thread = {Thread.CurrentThread.ManagedThreadId}");
-            //    //Sleep the loop for 10 miliseconds
-            //    Thread.Sleep(10);
-            //});
-            //Console.ReadLine();
+            Console.WriteLine("Parallel For Loop");
+            Parallel.For(0, number, count =>
+            {
+                Console.WriteLine($"value of count = {count}, thread = {Thread.CurrentThread.ManagedThreadId}");
+                //Sleep the loop for 10 miliseconds
+                Thread.Sleep(10);
+            });
+            Console.ReadLine();
         }
         private static void Runmillianitraton()
         {
